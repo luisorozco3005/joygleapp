10469bf6130034d0e91e3731809fc110ac3144cc
Luis Orozco <luisorozco3005@gmail.com>
2018-05-15 01:04:59 -0430

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+
+export class HomePage {
+
+  openMenu: boolean = false;
+  slides = [
+    {
+      title  : 'Bienvenido 1',
+      imagen : 'assets/imgs/1.jpg',   
+      precio : '$75.000',
+
+    },
+    {
+      title  : 'Bienvenido 2',
+      imagen : 'assets/imgs/2.jpg',  
+      precio : '$90.000',    
+
+    },
+    {
+      title  : 'Bienvenido 3',
+      imagen : 'assets/imgs/3.jpg',    
+      precio : '$125.000',  
+
+    }
+    
+  ];
+
+  tarjetas = [
+    {
+      title  : '5329 Debbt drive',
+      imagen : 'assets/imgs/4.jpg',   
+      city : 'Atlanta',
+      bedroom : '2',
+      bathroom : '3',
+
+  },
+  {
+    title  : '5329 Stuning Victorian',
+    imagen : 'assets/imgs/5.jpg',   
+    city : 'clayton',
+    bedroom : '4',
+    bathroom : '2',
+
+},
+{
+  title  : '5329 Hyaat Verian',
+  imagen : 'assets/imgs/6.jpg',   
+  city : 'Heisl',
+  bedroom : '3',
+  bathroom : '3',
+
+},
+];
+
+// Metodo de popup Menu
+
+togglePopupMenu() {
+  return this.openMenu = !this.openMenu;
+}
+
+goToCatalogue() {
+  alert('Catalogue clicked.');
+  this.togglePopupMenu();
+}
+
+goToFavorite() {
+  alert('Favorite clicked.');
+  this.togglePopupMenu();
+}
+
+goToAdvanced() {
+  alert('Advanced clicked.');
+  this.togglePopupMenu();
+}
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+ 
+
+}
